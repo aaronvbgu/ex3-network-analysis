c7ff15ee11e47e8db159ecbcea0e7df623e4c2b8
Aaron <aaronvine@gmail.com>
2016-05-07 16:04:02 +0300

    added part 2 data

--- README.md
+++ README.md
@@ -16,12 +16,33 @@ Grey's Anatomy Network of Sexual Relations
 **Communities(Short Random Walks)**
   1. Modularity: 0.5147059
   2. Sizes: 5 13 3 3 2 3 3
-  3.
-  ![grpah1](greys-anatomy-graph1.png)
+  3.![grpah1](greys-anatomy-graph1.png)
 
 
 **Communities(Girvan-Newman)**
   1. Modularity: 0.5774221
   2. Sizes: 8 5 4 4 5 3 3
-  3.
-  ![grpah1](greys-anatomy-graph2.png)
+  3.![grpah1](greys-anatomy-graph2.png)
+  
+
+
+IMDB Top Movies
+---------------
+
+
+**Centrality**
+  1. Max Betweeness: Morgan Freeman, Christoph Waltz, Leonardo DiCaprio and Samuel L. Jackson(48)
+  2. Max Closeness: Christoph Waltz, Leonardo DiCaprio, Samuel L. Jackson(0.0006973501)
+  3. Max Eigenvector: Vin Diesel, Paul Walker, Michelle Rodriguez, Jordana Brewster(1)
+
+
+**Communities(Short Random Walks)**
+  1. Modularity: 0.765
+  2. Sizes: 13 7 5 5 7 5 5
+  3.![grpah1](imdb-graph1.png)
+
+
+**Communities(Greedy Optimization)**
+  1. Modularity: 0.765
+  2. Sizes: 7 5 13 5 5 5 7
+  3.![grpah1](imdb-graph2.png)
--- network-analysis.R
+++ network-analysis.R
@@ -51,21 +51,68 @@ V(g)$color <- fc$membership
 V(g)$size <- 5
 plot(fc, g)
 
+
+library(igraph)
+library("XML")
+require(reshape2)
+
 #### Part 2 ####
 
-## Grey's Anatomy Network of Sexual Relations
-ga.data <- read.csv('ga_edgelist.csv', header=TRUE)
-g <- graph.data.frame(ga.data, directed=FALSE)
+## Imdb top movies
+
+movies <- c("tt0111161", # The Shawshank Redemption
+            "tt1853728", # Django Unchained
+            "tt2820852", # Furious Seven
+            "tt0405159", # Million Dollar Baby
+            "tt2637276", # Ted 2
+            "tt1483013", # Oblivion
+            "tt1905041", # Furious 6
+            "tt1013752", # Fast & Furious
+            "tt0463985", # The Fast and the Furious: Tokyo Drift
+            "tt0110912", # Pulp Fiction
+            "tt0361748", # Inglourious Basterds
+            "tt0993846"  # The Wolf of Wall Street
+)
+
+top_movies_data <- data.frame(matrix(NA, nrow=0, ncol=2))
+colnames(top_movies_data) <- c("Movie", "Cast")
+
+# crawling information from imdb
+for (i in 1:length(movies)) {
+  
+  ID <- movies[i]
+  URL <- paste0("http://www.imdb.com/title/", ID)
+  parsed.html <- htmlParse(URL)
+  Film <- xpathSApply(parsed.html, "//h1[@class='header']/span[@class='itemprop']", xmlValue)
+  if (is.null(Film)) 
+    Film =  xpathSApply(parsed.html, "//h1[@itemprop='name']", xmlValue)
+  parsed.html <- htmlParse(paste0(URL,"/fullcredits"))
+  Full_Cast <- xpathSApply(parsed.html, "//span[@itemprop='name']", xmlValue)
+  TopFiveCast <- head(Full_Cast, 5)
+  melt(TopFiveCast)
+  movie_data <- data.frame(Film = Film, TopFiveCast = TopFiveCast)
+  
+  # bind all movie data to one dataset
+  top_movies_data <- rbind(top_movies_data, movie_data) 
+}
+
+initial_g <- graph.data.frame(top_movies_data)
+# top_movies_data is the data frame with combined data of all comedy movies
+V(initial_g)$type <- bipartite.mapping(initial_g)$type
+g <- bipartite.projection(initial_g, which="true")
+g <- simplify(g)
 
 # get vertexes values
 V(g)$Closeness = closeness(g)
 V(g)$Betweenness = betweenness(g)
 V(g)$Eigenvector = evcent(g)$vector
+V(g)$Eigenvector
 
 # get max value
 maxBetweenness <- V(g)$name[which(V(g)$Betweenness == max(betweenness(g)))]
 maxCloseness <- V(g)$name[which(V(g)$Closeness == max(closeness(g)))]
 maxEigenvector <- V(g)$name[which(V(g)$Eigenvector == max(evcent(g)$vector))] 
+maxEigenvector
 
 # print
 cat("Max Betweenness belongs to: ", maxBetweenness  , ", value: ", max(betweenness(g)))
@@ -73,7 +120,6 @@ cat("Max closeness belongs to: ", maxCloseness, ", value: ", max(closeness(g)))
 cat("Max Eigenvector belongs to: ", maxEigenvector  , ", value: ", max(evcent(g)$vector))
 
 # graph properties
-g <- simplify(g)
 set.seed(100)
 lay <- layout.kamada.kawai(g)
 
@@ -89,9 +135,9 @@ V(g)$color <- fc$membership
 V(g)$size <- 5
 plot(fc, g)
 
-# Girvan-Newman community detection algorithm
-cat("Girvan-Newman:")
-fc <-  edge.betweenness.community(g)
+# community structure via greedy optimization of modularity
+cat("Greedy Optimization:")
+fc <-  fastgreedy.community(g)
 cat("Modularity: ", modularity(fc))
 cat("Sizes: ", sizes(fc))
 
